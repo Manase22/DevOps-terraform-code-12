62dd9231f578f6cf8a5b04dff1e3d4a496067803
manase22 <manasea22@gmail.com>
2024-04-02 22:18:31 -0400

    terraform -module

---
+++ meta-argument/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.39.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_iam_group" "gp1" {
+    name = "manager"
+    lifecycle {
+      prevent_destroy = false
+      
+      
+    }
+
+}
+
+resource "aws_iam_user" "usr1" {
+  name = "kwill2k"
+  depends_on = [ aws_iam_group.gp1 ]
+
+}
+
+
+resource "aws_instance" "sv1" {
+  ami = "ami-033a1ebf088e56e81"
+  instance_type = "t2.micro"
+  key_name = "wordpress"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
